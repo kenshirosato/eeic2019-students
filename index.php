5564ef9f2c0c967e85e6d04a0b19760cc80023f7
Kenshiro <sato-kenshiro906@g.ecc.u-tokyo.ac.jp>
2019-05-24 17:04:10 +0900

    認証追加

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-header('Location: /body.html');
+function require_auth() {
+    $AUTH_USER = 'eeic2019';
+    $AUTH_PASS = 'denkikei';
 
-?>
+    header('Cache-Control: no-cache, must-revalidate, max-age=0');
+    $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+    $is_not_authenticated = (
+        !$has_supplied_credentials ||
+        $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+        $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+    );
+    if ($is_not_authenticated) {
+        header('HTTP/1.1 401 Authorization Required');
+        header('WWW-Authenticate: Basic realm="Access denied"');
+        exit;
+    }
+}
+
+require_auth();
+
+header('Location: /body.html');
